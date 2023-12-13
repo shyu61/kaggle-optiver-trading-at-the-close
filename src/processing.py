e0428daff1c69cc3640eb77fe30b027dea8c02fa
shyu61 <ohs6261@gmail.com>
2023-12-14 00:29:15 +0900

    :watermelon: Add: some features

--- src/processing.py
+++ src/processing.py
@@ -70,18 +70,80 @@ def __add_index_wap(df: pl.DataFrame) -> pl.DataFrame:
     index_wap_df = df.group_by("time_id").agg(
         (pl.col("wap") * pl.col("weight")).sum().alias("index_wap")
     )
-    df = df.join(index_wap_df, on="time_id").sort("stock_id", "time_id").drop("weight")
+    df = df.join(index_wap_df, on="time_id").sort("stock_id", "time_id")
+    return df
+
+
+def __add_rolling(df: pl.DataFrame) -> pl.DataFrame:
+    # fmt: off
+    df = df.with_columns(
+        pl.col("wap").rolling_mean(3).over("stock_id", "date_id").cast(pl.Float32).alias("wap_30s_rolling_mean"),  # noqa
+        pl.col("wap").rolling_mean(6).over("stock_id", "date_id").cast(pl.Float32).alias("wap_60s_rolling_mean"),  # noqa
+        pl.col("wap").rolling_std(3).over("stock_id", "date_id").cast(pl.Float32).alias("wap_30s_rolling_std"),  # noqa
+        pl.col("wap").rolling_std(6).over("stock_id", "date_id").cast(pl.Float32).alias("wap_60s_rolling_std"),  # noqa
+
+        pl.col("wap").diff().over("stock_id", "date_id").cast(pl.Float32).alias("wap_diff"),  # noqa
+        pl.col("wap").diff().rolling_mean(3).over("stock_id", "date_id").cast(pl.Float32).alias("wap_diff_30s_rolling_mean"),  # noqa
+        pl.col("wap").diff().rolling_mean(6).over("stock_id", "date_id").cast(pl.Float32).alias("wap_diff_60s_rolling_mean"),  # noqa
+
+        (pl.col("wap").shift(1).over("stock_id", "date_id") / pl.col("wap")).cast(pl.Float32).alias("wap_shift_1_ratio"),  # noqa
+        (pl.col("wap").shift(2).over("stock_id", "date_id") / pl.col("wap")).cast(pl.Float32).alias("wap_shift_2_ratio"),  # noqa
+        (pl.col("wap").shift(3).over("stock_id", "date_id") / pl.col("wap")).cast(pl.Float32).alias("wap_shift_3_ratio"),  # noqa
+        (pl.col("wap").shift(4).over("stock_id", "date_id") / pl.col("wap")).cast(pl.Float32).alias("wap_shift_4_ratio"),  # noqa
+        (pl.col("wap").shift(5).over("stock_id", "date_id") / pl.col("wap")).cast(pl.Float32).alias("wap_shift_5_ratio"),  # noqa
+        (pl.col("wap").shift(6).over("stock_id", "date_id") / pl.col("wap")).cast(pl.Float32).alias("wap_shift_6_ratio"),  # noqa
+
+        pl.col("wap").shift(1).over("stock_id", "date_id").cast(pl.Float32).alias("wap_shift_1"),  # noqa
+        pl.col("wap").shift(2).over("stock_id", "date_id").cast(pl.Float32).alias("wap_shift_2"),  # noqa
+        pl.col("wap").shift(3).over("stock_id", "date_id").cast(pl.Float32).alias("wap_shift_3"),  # noqa
+        pl.col("wap").shift(4).over("stock_id", "date_id").cast(pl.Float32).alias("wap_shift_4"),  # noqa
+        pl.col("wap").shift(5).over("stock_id", "date_id").cast(pl.Float32).alias("wap_shift_5"),  # noqa
+        pl.col("wap").shift(6).over("stock_id", "date_id").cast(pl.Float32).alias("wap_shift_6"),  # noqa
+    )
+    # closing auction中で、sizeがどう変化しているかの情報を追加する
+    size_cols = ["imbalance_size", "matched_size", "bid_size", "ask_size"]
+    for col in size_cols:
+        df = df.with_columns(
+            pl.col(col).rolling_mean(3).over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_30s_rolling_mean"),  # noqa
+            pl.col(col).rolling_mean(6).over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_60s_rolling_mean"),  # noqa
+            pl.col(col).rolling_std(3).over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_30s_rolling_std"),  # noqa
+            pl.col(col).rolling_std(6).over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_60s_rolling_std"),  # noqa
+
+            pl.col(col).diff().over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_diff"),
+            pl.col(col).diff().rolling_mean(3).over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_diff_30s_rolling_mean"),  # noqa
+            pl.col(col).diff().rolling_mean(6).over("stock_id", "date_id").cast(pl.Float32).alias(f"{col}_diff_60s_rolling_mean"),  # noqa
+        )
+    # fmt: on
+    return df
+
+
+def __add_prev_date(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    前日の同時刻の情報、1時刻先の情報を特徴量として加える。
+    また、前日の同時刻との比率
+    """
+    df = (
+        df.group_by("stock_id", "seconds_in_bucket")
+        .map_groups(
+            lambda group: group.sort("date_id").with_columns(
+                pl.col("wap").shift(1).alias("prev_date_same_sec_wap"),
+            )
+        )
+        .sort("stock_id", "seconds_in_bucket")
+    )
     return df
 
 
 def feature_engineering(
     df: pl.DataFrame, maintain_stock_id: bool = False
 ) -> pl.DataFrame:
-    drop_cols = ["date_id", "row_id", "time_id"]
+    drop_cols = ["date_id", "row_id"]
     if not maintain_stock_id:
         drop_cols.append("stock_id")
 
     df = __add_index_wap(df)
+    df = __add_rolling(df)
+    df = __add_prev_date(df)
 
     df = df.with_columns(
         (
