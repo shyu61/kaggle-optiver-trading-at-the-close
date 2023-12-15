95729b8a25d30352f0e8edef477e8b7a2c0807fa
shyu61 <ohs6261@gmail.com>
2023-12-15 23:19:53 +0900

    :wrench: Fix: paperspace script to setup talib

--- paperspace/restart.sh
+++ paperspace/restart.sh
@@ -14,6 +14,16 @@ python3.10 get-pip.py --user
 echo "alias pip='python3.10 -m pip'" >> /root/.bashrc
 rm get-pip.py
 
+# setup ta-lib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure
+make
+make install
+cd ..
+rm ta-lib-0.4.0-src.tar.gz
+
 # install dependencies
 awk '/-e file:\./ {flag=1; next} flag' "./kaggle-optiver-trading-at-the-close/requirements.lock" > "requirements.txt"
 python3.10 -m pip install -r requirements.txt
