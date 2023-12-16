8e26ca3685ad610e1cdf543b4f983f9ab95505bd
shyu61 <ohs6261@gmail.com>
2023-12-16 21:00:17 +0900

    :+1: Update: inference to adapt to cv models ensemble

--- run/inference.py
+++ run/inference.py
@@ -11,7 +11,8 @@ from omegaconf import DictConfig
 def load_models(cfg: DictConfig) -> Dict:
     all_models = {}
     for kind in cfg.model.kinds:
-        all_models[kind] = joblib.load(Path(cfg.model.dir) / f"{kind}_models.joblib")
+        all_models[kind] = [joblib.load(Path(cfg.model.dir) / f"{kind}_models.joblib")]
+        all_models[kind].extend(joblib.load(Path(cfg.model.dir) / f"{kind}_cv_models.joblib"))
     return all_models
 
 
@@ -52,10 +53,16 @@ def main(cfg: DictConfig):
         df = preprocessing(df)
         df = feature_engineering(df)
 
-        preds = []
+        preds = np.zeros(len(df))
+        weight = 1 / (cfg.model.cv_size + 2)
         for kind in cfg.model.kinds:
-            preds.append(models[kind].predict(df.to_pandas()))
-        sample_prediction["target"] = np.mean(preds, 0)
+            for i in range(len(models[kind])):
+                if i == 0:
+                    # whole dataset modelはweightを2倍にする
+                    weight = weight * 2
+                pred = models[kind][i].predict(df.to_pandas())
+                preds += pred * weight
+        sample_prediction["target"] = preds
         env.predict(sample_prediction)
 
 
